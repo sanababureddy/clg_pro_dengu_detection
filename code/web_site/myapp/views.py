6aa8d21ede7dd26fb13dea32fa3513f8cba58521
Patil Lakshminarayanamma <lakshmi10.patil@gmail.com>
2020-02-18 16:22:42 +0530

    project code completed

--- code/web_site/myapp/views.py
+++ code/web_site/myapp/views.py
@@ -27,10 +27,17 @@ def predict(request):
     platelets_count = request.GET["platelets_count"]
     values = str(fever)+','+str(vomiting)+','+str(nausea)+','+str(vomiting_blood)+','+str(body_pains)+','+str(pain_behind_eyes)+','+str(joint_pains)+','+str(chill)+','+str(headache)+','+str(swollen_glands)+','+str(rashes)+','+str(abdominal_pain)+','+str(ble_nose)+','+str(ble_mouth)+','+str(fatigue)+','+str(red_eyes)+','+str(platelets_count)
     output = script_function(values)
-    # lines = str(output.stdout.splitlines())
+    lines = str(output.stdout.splitlines())
     # result_text = lines.strip('[b"the recieved values are:').strip(']"]').split(', ')
     # result_text = str(result_text[1].strip(']"'))
-    return render(request, "predict.html",{'result':output})
+    prediction_result = lines.strip("[b'").strip("']")
+    if prediction_result == 'True':
+        prediction_result = '<h1 style="text-align: center; color: red;">Patient infected with Dengue virus</h1>'
+    elif prediction_result == 'False':
+        prediction_result = '<h1 style="text-align: center; color: green;">Patient does not have Dengue</h1>'
+    else:
+        prediction_result = "Unknown"
+    return HttpResponse(prediction_result)
 
 def script_function(values):
-  return subprocess.run(['python', 'C:/Users/sanab/Documents/clg_pro_dengu_detection/code/predict.py', values],stdout=subprocess.PIPE)  
+  return subprocess.run(['python', 'C:/Users/tejak/Desktop/Project@ML/github/code/predict.py', values],stdout=subprocess.PIPE)  
--- code/web_site/predict.py
+++ code/web_site/predict.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import sys
+#import seaborn as sns
 #ignoring feature warnings
 import warnings
 warnings.filterwarnings('ignore')
 data=pd.read_csv("C:/Users/tejak/Desktop/Project@ML/github/code/dataset.csv")
 data.drop(columns={'id'},inplace=True) #Deleting id
-get={'no':0,'yes':1,'low':2,'medium':3,'high':4} 
+get={'yes':1,'medium':1,'no':0,'high':2,'low':0} 
 data.vomiting=data.vomiting.map(get)
 data.nausea=data.nausea.map(get)
 data.vomiting_blood=data.vomiting_blood.map(get)
@@ -23,37 +24,77 @@ data.fatigue=data.fatigue.map(get)
 data.red_eyes=data.red_eyes.map(get)
 data.dengue=data.dengue.map(get)
 data=data[:225] #using 225 rows
+## to split the data into ratio of 75% and 25% to train model and test the model
+
 from sklearn.model_selection import train_test_split
+
 new_data=data.drop(columns={'dengue'})
 X_train,X_test,y_train,y_test=train_test_split(new_data,data['dengue'],random_state=7)
+#The data is preprocessing using sklearn.preprocessing.Normalizer
 from sklearn.preprocessing import Normalizer
+
 norm=Normalizer()
 X_train_normal=norm.transform(X_train)
 X_test_normal=norm.transform(X_test)
+#print(X_test_normal)
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import LogisticRegression
+
+
 RF_params = {'n_estimators':[10,50,100]}
 DTC_params = {'criterion':['entropy'], 'max_depth':[10, 50, 100]}
 LR_params = {'C':[0.001, 0.1, 1, 10, 100]}
 from sklearn.model_selection import KFold, cross_val_score
 from sklearn.model_selection import GridSearchCV
 models=[]
+
 models.append(('DTC', DecisionTreeClassifier(), DTC_params))
+
 models.append(('LR', LogisticRegression(), LR_params))
+# from tqdm import tqdm
+# results=[]
+# names=[]
+# scoring='accuracy' 
+# for name, model, params in tqdm(models):
+#     kfold = KFold(len(X_train_normal), random_state=7, shuffle=True)
+#     model_grid = GridSearchCV(model, params)
+#     cv_results = cross_val_score(model_grid, X_train_normal, y_train, cv = kfold, scoring=scoring)
+#     results.append(cv_results)
+#     names.append(name)
+#     msg = "Cross Validation Accuracy %s: Accarcy: %f SD: %f" % (name, cv_results.mean(), cv_results.std())
+#     #print(msg)
+#The accuracy score obtained without using GridSearchCV
+
 from sklearn.metrics import make_scorer, accuracy_score, fbeta_score
+
 clf=RandomForestClassifier(n_estimators=100, random_state=42)
 clf.fit(X_train_normal,y_train)
 pred=clf.predict(X_test_normal)
 accuracy_score(pred,y_test)
 from sklearn.metrics import make_scorer, accuracy_score, fbeta_score
+
 clf = DecisionTreeClassifier(random_state=42)
+
+# TODO: Create the parameters list you wish to tune
 parameters = {'criterion':['entropy'], 'max_depth':[10, 50, 100]}
+
+# TODO: Make an fbeta_score scoring object
+
+
+# TODO: Perform grid search on the classifier using 'scorer' as the scoring method
 grid_obj = GridSearchCV(clf,parameters,scoring='accuracy')
+
+# TODO: Fit the grid search object to the training data and find the optimal parameters
 grid_fit = grid_obj.fit(X_train_normal,y_train)
+
+# Get the estimator
 best_clf = grid_fit.best_estimator_
+
+# Make predictions using the unoptimized and model
 predictions = (clf.fit(X_train_normal, y_train)).predict(X_test_normal)
 best_predictions = best_clf.predict(X_test_normal)
+
 def Predict(fever, vomiting, nausea, vomiting_blood, body_pains, pain_behind_eyes, joint_pains, chill, headache, swollen_glands, rashes, abdominal_pain, ble_nose, ble_mouth, fatigue, red_eyes, platelets_count):
     normilized_value = norm.transform([[fever, vomiting, nausea, vomiting_blood, body_pains, pain_behind_eyes, joint_pains, chill, headache, swollen_glands, rashes, abdominal_pain, ble_nose, ble_mouth, fatigue, red_eyes, platelets_count]]);
     element = str(normilized_value[[0]])
@@ -65,23 +106,9 @@ def Predict(fever, vomiting, nausea, vomiting_blood, body_pains, pain_behind_eye
         return False
     else:
         return "Unknown"
+#prediction function calling
+#Enter the values in the sequence fever, vomiting, nausea, vomiting_blood, body_pains, pain_behind_eyes, joint_pains, chill, headache, swollen_glands, rashes, abdominal_pain, ble_nose, ble_mouth, fatigue, red_eyes, platelets_count
 recieved_values = str(sys.argv)
-rv = recieved_values.strip('[').strip(']').strip("'").strip(" '").split(',')
-fever = float(rv[1].strip(" '"))
-vomiting = int(rv[2])
-nausea =  int(rv[3])
-vomiting_blood =  int(rv[4])
-body_pains =  int(rv[5])
-pain_behind_eyes =  int(rv[6])
-joint_pains =  int(rv[7])
-chill =  int(rv[8])
-headache =  int(rv[9])
-swollen_glands =  int(rv[10])
-rashes =  int(rv[11])
-abdominal_pain =  int(rv[12])
-ble_nose =  int(rv[13])
-ble_mouth =  int(rv[14])
-fatigue =  int(rv[15])
-red_eyes =  int(rv[16])
-platelets_count =  int(rv[17])
-print(Predict(fever, vomiting, nausea, vomiting_blood, body_pains, pain_behind_eyes, joint_pains, chill, headache, swollen_glands, rashes, abdominal_pain, ble_nose, ble_mouth, fatigue, red_eyes, platelets_count))
+
+print(type(recieved_values))
+#print(Predict(100,1,0,1,2,1,0,1,1,1,0,1,1,0,1,0,17000))
